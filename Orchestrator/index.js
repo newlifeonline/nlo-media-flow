d7562ae438750738fdbd32d8be3824109a7b113b
Casey Collins <casey.collins@dcsg.com>
2019-02-24 15:31:06 -0500

    Adjusting input

--- Orchestrator/index.js
+++ Orchestrator/index.js
@@ -1,7 +1,7 @@
 const df = require('durable-functions');
 
 module.exports = df.orchestrator(function* (context) {
-    const submission = context.bindings.input;
+    const submission = context.df.getInput();
     const vimeoResult = yield context.df.callActivity('VimeoNotifier', submission.entity);
 
     submission.entity.vimeoUri = vimeoResult;
