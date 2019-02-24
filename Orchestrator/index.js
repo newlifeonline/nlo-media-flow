5efe85d9ff6e2496a5b2d2063be2dd306e037900
Casey Collins <casey.collins@dcsg.com>
2019-02-24 18:12:54 -0500

    trying to fix mutability error

--- Orchestrator/index.js
+++ Orchestrator/index.js
@@ -3,7 +3,11 @@ const df = require('durable-functions');
 module.exports = df.orchestrator(function* (context) {
     let submission = context.df.getInput();
     const vimeoResult = yield context.df.callActivity('VimeoNotifier', submission.entity);
-    submission.entity.vimeoUri = vimeoResult;
+    context.log(vimeoResult);
+
+    let entity = submission.entity;
+    entity.vimeoUri = vimeoResult;
+    submission.entity = entity;
 
     yield context.df.callActivity('SaveMediaSubmission', submission);
     return context.instanceId;
