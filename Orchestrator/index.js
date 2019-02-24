f2bffae593c5b02c7cfd377bf4661e35471b9c7d
Casey Collins <casey.collins@dcsg.com>
2019-02-24 17:59:01 -0500

    updating variable

--- Orchestrator/index.js
+++ Orchestrator/index.js
@@ -1,9 +1,8 @@
 const df = require('durable-functions');
 
 module.exports = df.orchestrator(function* (context) {
-    const submission = context.df.getInput();
+    let submission = context.df.getInput();
     const vimeoResult = yield context.df.callActivity('VimeoNotifier', submission.entity);
-
     submission.entity.vimeoUri = vimeoResult;
 
     yield context.df.callActivity('SaveMediaSubmission', submission);
