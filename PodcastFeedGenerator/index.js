ee8b8c2c7ac910554ba1b105dae300e56366d013
Casey Collins <casey.collins@dcsg.com>
2019-02-25 20:11:29 -0500

    Adding podcast generator function

---
+++ PodcastFeedGenerator/index.js
@@ -0,0 +1,17 @@
+module.exports = async function (context, req) {
+    const m = context.bindings.inputTable
+        .sort((a, b) => {
+            return b - a;
+        })
+        .take(100);
+
+    const xml = PodcastFeed.generateXML(m);
+
+    context.res = {
+        'headers': {
+            'Content-Type': 'text/xml'
+        },
+        'body': xml,
+        'isRaw': true
+    };
+};
---
+++ PodcastFeedGenerator/podcast-feed.js
@@ -0,0 +1,45 @@
+const Podcast = require('podcast');
+class PodcastFeed {
+    static generateXML(mediaItems) {
+        let feed = new Podcast(getFeedHeader());
+        mediaItems.forEach(m => {
+            feed.addItem(m);
+        });
+
+        return feed.buildXML();
+    }
+
+    static getFeedHeader() {
+        return {
+            title: data.title,
+            description: data.desciption,
+            feedUrl: data.feedUrl,
+            categories: data.categories,
+            pubDate: data.pubDate,
+            language: 'en-GB',
+            copyright: 'All content copyright New Life Christian Ministries Inc.',
+            itunesAuthor: 'New Life',
+            itunesSubTitle: 'This is the podcast of New Life Christian Ministries in Saxonburg, PA!',
+            itunesExplicit: false,
+            itunesSummary: 'Weekend messages from New Life Christian Ministries in Saxonburg. Ordinary people serving an EXTRAORDINARY GOD!',
+            itunesImage: 'http://www.newlifexn.com/podcasts/iTunes-Cover-Art/images/NL18.jpg',
+        };
+    }
+
+    static getFeedItem(m) {
+        return {
+            title: m.title,
+            description: m.description,
+            enclosure: {
+                url: ''
+            },
+            date: e.eventDate,
+            guid: '',
+            itunesDuration: 0,
+            itunesExplicit: false,
+            itunesKeywords: ['New Life', 'New Life Christian Ministries', 'Saxonburg', 'Church', 'Bible', 'God', 'Preaching', 'Teaching', 'Bible Teaching', 'Bible Preaching', 'Chris Marshall', 'Brad French', 'Mark Lutz', 'New', 'Life', 'Jesus', 'God', 'Holy Spirit', 'Christianity', 'Christian', 'Faith']
+        };
+    }
+}
+
+module.exports = PodcastFeed;
