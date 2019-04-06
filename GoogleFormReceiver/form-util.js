6fe8571fa168c33f2bd9ac76e8d04ff8ca823d57
Casey Collins <casey.collins@dcsg.com>
2019-04-05 21:01:16 -0400

    spliting image to audio and video image

--- GoogleFormReceiver/form-util.js
+++ GoogleFormReceiver/form-util.js
@@ -2,20 +2,24 @@ class FormUtil {
     constructor() { }
 
     static parse(formData) {
-        let imageId = formData.image_file.length > 0 ? 
-                            formData.image_file[0] : '';
+        
         let audioId = formData.audio_file.length > 0 ? 
                             formData.audio_file[0] : '';
+        let audioImageId = formData.audio_image_file.length > 0 ? 
+                            formData.audio_image_file[0] : '';
         let videoId = formData.video_file.length > 0 ? 
                             formData.video_file[0] : '';
+        let videoImageId = formData.video_image_file.length > 0 ? 
+                            formData.video_image_file[0] : '';
         return {
             title: formData.title,
             description: formData.description,
             eventDate: formData.event_date,
-            googleImageFileId: imageId,
             googleAudioFileId: audioId,
+            googleAudioImageFileId: audioImageId,
             audioId: audioId,
             googleVideoFileId: videoId,
+            googleVideoImageFileId: videoImageId,
             tagsCSV: formData.tags.join(',')
         };
     }
