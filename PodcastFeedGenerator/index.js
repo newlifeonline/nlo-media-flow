1f95f39afcd7d419bd5596595439007ce9315d24
Casey Collins <casey.collins@dcsg.com>
2019-06-13 21:10:30 -0400

    adjusting date sort for podcast feed

--- PodcastFeedGenerator/index.js
+++ PodcastFeedGenerator/index.js
@@ -3,7 +3,7 @@ const PodcastFeed = require('./podcast-feed');
 module.exports = async function (context) {
     const m = context.bindings.inputTable
         .sort((a, b) => {
-            return b - a;
+            return new Date(b.eventDate) - new Date(a.eventDate);
         });
 
     const xml = PodcastFeed.generateXML(m);
