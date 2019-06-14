ba7fc7ad8891a8d35d97e2b7109197b58e694cdb
Casey Collins <casey.collins@dcsg.com>
2019-06-13 21:29:36 -0400

    adjusting reindexer

--- TagReindexer/index.js
+++ TagReindexer/index.js
@@ -7,21 +7,31 @@ const baseBlobUrl = process.env["BASE_BLOB_URL"];
 module.exports = async function (context, req) {
     const tableName = 'TagChannels';
     const partitionKey = 'nlo';
-    const submission = context.bindings.input;
-    const tags = submission.entity.tagsCSV.split(',');
-    const vimeoId = submission.entity.vimeoId || null;
-    const audioUrl =  submission.entity.audioId ? 
-        `${baseBlobUrl}/podcasts/${submission.entity.audioId}.mp3` : null;
+    const submissionsTable = context.bindings.inputTable;
 
-    const f = {
-        id: submission.id,
-        title: submission.entity.title,
-        description: submission.entity.description,
-        eventDate: submission.entity.eventDate,
-        audioUrl: audioUrl,
-        vimeoId: vimeoId,
-        tags: tags
-    }
+    const masterTagList = [];
+
+    const submissions = submissionsTable.map(s => {
+        const tags = s.entity.tagsCSV.split(',');
+        const vimeoId = s.entity.vimeoId || null;
+        const audioUrl =  s.entity.audioId ? 
+            `${baseBlobUrl}/podcasts/${s.entity.audioId}.mp3` : null;
+
+        tags.forEach(tag => {
+            if (masterTagList.find(t => t !== tag))
+                masterTagList.push(tag);
+        });
+
+        return {
+            id: s.id,
+            title: s.entity.title,
+            description: s.entity.description,
+            eventDate: s.entity.eventDate,
+            audioUrl: audioUrl,
+            vimeoId: vimeoId,
+            tags: tags
+        };
+    });    
     
     const tableService = azure.createTableService(connStr);
     await new Promise((resolve, reject) => {
@@ -36,53 +46,28 @@ module.exports = async function (context, req) {
         });
     });
     
-    const promises = tags.map(t => {
+    const promises = masterTagList.map(t => {
         return new Promise((resolve, rej) => {
             tableService.retrieveEntity(tableName, partitionKey, t, (error, result, response) => {
-                let entity = {};
-                if (error || !result) {
-                    // make a new one
-                    entity = {
-                        PartitionKey: partitionKey,
-                        RowKey: t,
-                        media: JSON.stringify([f])
-                    }
-                } else {
-                    entity = response.body;
-                    let medias = [];
-                    if (entity.media && entity.media.length > 0) {
-                        medias = JSON.parse(entity.media);
-                        // if dup, replace it
-                        let foundInArray = false;
-                        medias = medias.map((item) => {
-                            if (item.id === f.id) {
-                                foundInArray = true;
-                                return f;
-                            } else {
-                                return item;
-                            }
-                        });
-
-                        if (!foundInArray) {
-                            medias.push(f);
-                        }
-                        medias = medias.sort((a,b) => {
-                            let aDate = new Date(a.eventDate);
-                            let bDate = new Date(b.eventDate);
-                            if (aDate > bDate)
-                                return -1;
-                            if (aDate < bDate)
-                                return 1;
-                            return 0;
-                        });
-                    } else {
-                        // new array
-                        medias = [f];
-                    }
-                    
-                    entity.media = JSON.stringify(medias);
+                let medias = submissions.filter(s => {
+                    return s.tags && s.tags.find(subTag => subTag === t);
+                });
+                medias = medias.sort((a,b) => {
+                    let aDate = new Date(a.eventDate);
+                    let bDate = new Date(b.eventDate);
+                    if (aDate > bDate)
+                        return -1;
+                    if (aDate < bDate)
+                        return 1;
+                    return 0;
+                });
+                // make a new one
+                let entity = {
+                    PartitionKey: partitionKey,
+                    RowKey: t,
+                    media: JSON.stringify(medias)
                 }
-
+                
                 tableService.insertOrReplaceEntity(tableName, entity, (e, resl, resp) => {
                     if (e) {
                         context.log.error(e);
