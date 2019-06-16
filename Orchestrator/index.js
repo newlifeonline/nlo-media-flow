0e4cbabb969013336ffd716c54e5de74b3f61ba4
Casey Collins <casey.collins@dcsg.com>
2019-06-16 13:30:23 -0400

    updating orchestrator for handling no video upload and saving other files to blob

--- Orchestrator/index.js
+++ Orchestrator/index.js
@@ -2,13 +2,27 @@ const df = require('durable-functions');
 
 module.exports = df.orchestrator(function* (context) {
     const submission = context.df.getInput();
-    const vimeoResult = yield context.df.callActivity('VimeoNotifier', submission.entity);
-    
-    submission.entity.vimeoUri = vimeoResult.videoUri;
-    submission.entity.vimeoId = vimeoResult.videoId;
+
+    if (submission.entity.googleVideoFileId) {
+        const vimeoResult = yield context.df.callActivity('VimeoNotifier', submission.entity);
+        submission.entity.vimeoUri = vimeoResult.videoUri;
+        submission.entity.vimeoId = vimeoResult.videoId;
+    } else if (submission.entity.videoId) {
+        submission.entity.vimeoUri = `/videos/${submissoin.entity.videoId}`;
+    }
+    // TODO - update vimeo with title and desc
 
     yield context.df.callActivity('SaveMediaSubmission', submission);
-    yield context.df.callActivity('TransferGoogleFileToBlob', submission.entity.googleAudioFileId);
+
+    if (submission.entity.googleAudioFileId)
+        yield context.df.callActivity('TransferGoogleFileToBlob', submission.entity.googleAudioFileId);
+    
+    if (submission.entity.googleAudioImageFileId)
+        yield context.df.callActivity('TransferGoogleFileToBlob', submission.entity.googleAudioImageFileId);
+    
+    if (submission.entity.googleVideoImageFileId)
+        yield context.df.callActivity('TransferGoogleFileToBlob', submission.entity.googleVideoImageFileId);
+    
     yield context.df.callActivity('PodcastFeedGenerator');
     yield context.df.callActivity('TagIndexer', submission);
 
