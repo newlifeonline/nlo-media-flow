ebf8f31d80ea860b9414484fec2dacea7891efa3
Casey Collins <casey.collins@dcsg.com>
2019-06-18 13:37:10 -0400

    fixing length of undefined error when video file is not specified

--- GoogleFormReceiver/form-util.js
+++ GoogleFormReceiver/form-util.js
@@ -3,14 +3,15 @@ class FormUtil {
 
     static parse(formData) {
         
-        let audioId = formData.audio_file.length > 0 ? 
+        let audioId = formData.audio_file && formData.audio_file.length > 0 ? 
                             formData.audio_file[0] : '';
-        let audioImageId = formData.audio_image.length > 0 ? 
+        let audioImageId = formData.audio_image && formData.audio_image.length > 0 ? 
                             formData.audio_image[0] : '';
-        let videoId = formData.video_file.length > 0 ? 
+        let videoId = formData.video_file && formData.video_file.length > 0 ? 
                             formData.video_file[0] : '';
-        let videoImageId = formData.video_image.length > 0 ? 
+        let videoImageId = formData.video_image && formData.video_image.length > 0 ? 
                             formData.video_image[0] : '';
+        
         let vimeoId = formData.vimeo_id && formData.vimeo_id > 0 ? formData.vimeo_id : '';
 
         return {
