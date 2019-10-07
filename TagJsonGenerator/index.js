ab0ac71b6185ffefaf9f09b430c93685c9d9878b
Casey Collins <casey.collins@dcsg.com>
2019-10-06 20:07:56 -0400

    replacing channel name spaces with  underscores

--- TagJsonGenerator/index.js
+++ TagJsonGenerator/index.js
@@ -29,8 +29,9 @@ module.exports = async function (context) {
     const promises = tags.map(t => {
         return new Promise((resolve, reject) => {
             tableService.retrieveEntity(tableName, partitionKey, t, (error, result, response) => {
-                const blobName = `${t.toLowerCase()}.json`;
-                const blockName = `${t.toLowerCase()}`;
+                const name = t.toLowerCase().replace(' ', '_');
+                const blobName = `${name}.json`;
+                const blockName = `${name}`;
                 const entity = response.body;
 
                 if (entity.media) {
